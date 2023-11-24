00208866c49a0a7bc98234ee205a68a41ce86c22
Zer02z2 <zongzechan@gmail.com>
2023-11-24 00:26:43 -0500

    Deleted First Person Control
    and a little bit of clean up

--- src/index.js
+++ src/index.js
@@ -305,73 +305,6 @@ function generateHeight(width, height) {
 
 }
 
-//---------------------------------------------------------------------------------
-
-function generateTexture(data, width, height) {
-
-  let context, image, imageData, shade;
-
-  const vector3 = new THREE.Vector3(0, 0, 0);
-
-  const sun = new THREE.Vector3(1, 1, 1);
-  sun.normalize();
-
-  const canvas = document.createElement('canvas');
-  canvas.width = width;
-  canvas.height = height;
-
-  context = canvas.getContext('2d');
-  context.fillStyle = '#000';
-  context.fillRect(0, 0, width, height);
-
-  image = context.getImageData(0, 0, canvas.width, canvas.height);
-  imageData = image.data;
-
-  for (let i = 0, j = 0, l = imageData.length; i < l; i += 4, j++) {
-
-    vector3.x = data[j - 2] - data[j + 2];
-    vector3.y = 2;
-    vector3.z = data[j - width * 2] - data[j + width * 2];
-    vector3.normalize();
-
-    shade = vector3.dot(sun);
-
-    imageData[i] = (96 + shade * 128) * (0.5 + data[j] * 0.007);
-    imageData[i + 1] = (32 + shade * 96) * (0.5 + data[j] * 0.007);
-    imageData[i + 2] = (shade * 96) * (0.5 + data[j] * 0.007);
-  }
-
-  context.putImageData(image, 0, 0);
-
-  // Scaled 4x ??
-  const canvasScaled = document.createElement('canvas');
-  canvasScaled.width = width * 4;
-  canvasScaled.height = height * 4;
-
-  context = canvasScaled.getContext('2d');
-  context.scale(4, 4);
-  context.drawImage(canvas, 0, 0);
-
-  image = context.getImageData(0, 0, canvasScaled.width, canvasScaled.height);
-  imageData = image.data;
-
-  for (let i = 0, l = imageData.length; i < l; i += 4) {
-
-    const v = ~ ~(Math.random() * 5);
-
-    imageData[i] += v;
-    imageData[i + 1] += v;
-    imageData[i + 2] += v;
-
-  }
-
-  context.putImageData(image, 0, 0);
-
-  return canvasScaled;
-
-}
-
-
 //--------------------------------------------------------------------------------
 //--------------------------------------------------------------------------------
 
@@ -381,9 +314,21 @@ function animate() {
 
   requestAnimationFrame(animate);
 
+  updateControls();
+
+  renderer.render(scene, camera);
+  //controls.update(clock.getDelta());
+
+};
+
+//-----------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------
+
+function updateControls() {
+
   const time = performance.now();
 
-  let lookAtVector = new THREE.Vector3(0,0, -1);
+  let lookAtVector = new THREE.Vector3(0, 0, -1);
   lookAtVector.applyQuaternion(camera.quaternion);
   lookAtVector.normalize();
 
@@ -404,7 +349,7 @@ function animate() {
     direction.normalize(); // this ensures consistent movements in all directions
 
     if (moveForward || moveBackward) {
-      
+
       velocity.z -= direction.z * 4000.0 * delta;
       velocity.y -= direction.y * 4000.0 * delta;
 
@@ -422,14 +367,7 @@ function animate() {
 
   prevTime = time;
 
-  renderer.render(scene, camera);
-  //controls.update(clock.getDelta());
-
-};
-
-//-----------------------------------------------------------------------------------
-//-----------------------------------------------------------------------------------
-
+}
 
 function makePools() {
   // box
